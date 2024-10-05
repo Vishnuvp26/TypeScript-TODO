d13d44e97fe6a297132a9e43393dbb0fd0689286
Vishnu Mohan <vishnumohanmfc@gmail.com>
2024-10-05 20:22:00 +0530

    task finished

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import path from 'path'
+import router from './router/router';
+
+dotenv.config();
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, '../src/public')));
+app.use(express.static(path.join(__dirname, '../dist/public')))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/', router);
+
+app.listen(3000, () => console.log('server started'));
